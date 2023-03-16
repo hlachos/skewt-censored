5630eecfda277fac5204a3573d681bc19b4df268
mso1980 <128004373+mso1980@users.noreply.github.com>
2023-03-15 20:26:14 -0400

    Update ApplicationsToRun.R
    File updated: ApplocationsToRun.R

--- Diagnostics/ApplicationsToRun.R
+++ Diagnostics/ApplicationsToRun.R
@@ -46,25 +46,23 @@ benchmark_GD_ST <- 2*(p+2)/n
 par(mfrow=c(2,2))
 plot(CD_N$GD, ylim = c(0, 1.4), main="N", xlab="Index", ylab="GD", type="h")
 abline(h = benchmark_GD_N, lty = 3)
-plot(CD_SN$GD, ylim = c(0, 0.2), main="SN", xlab="Index", ylab="GD", type="h")
+plot(CD_SN$GD, ylim = c(0, 0.1), main="SN", xlab="Index", ylab="GD", type="h")
 abline(h = benchmark_GD_SN, lty = 3)
-plot(CD_T$GD, ylim = c(0, 0.2), main="T", xlab="Index", ylab="GD", type="h")
+plot(CD_T$GD, ylim = c(0, 0.1), main="T", xlab="Index", ylab="GD", type="h")
 abline(h = benchmark_GD_T, lty = 3)
-plot(CD_ST$GD, ylim = c(0, 0.2), main="ST", xlab="Index", ylab="GD", type="h")
+plot(CD_ST$GD, ylim = c(0, 0.1), main="ST", xlab="Index", ylab="GD", type="h")
 abline(h = benchmark_GD_ST, lty = 3)
 
 ##  Mahalanobis versus Weights
-maha_N  <- (est_N$yhat -x%*%est_N$theta[1:p])^2/est_N$theta[p+1]
-maha_SN <- (est_SN$yhat-x%*%est_SN$theta[1:p])^2/est_SN$theta[p+1]
 maha_T  <- (est_T$yhat -x%*%est_T$theta[1:p])^2/est_T$theta[p+1]
 maha_ST <- (est_ST$yhat-x%*%est_ST$theta[1:p])^2/est_ST$theta[p+1]
 
 ### Figure. Mahalanobis versus Weights
 par(mfrow=c(1,2))
-plot(maha_T[cc==0], est_T$E00[cc==0], xlim=c(0,92), main="T", xlab = "Mahalanobis distance", ylab ="Weights", pch=3)
+plot(maha_T[cc==0], est_T$E00[cc==0], xlim = c(min(maha_T),max(maha_T)), main="T", xlab = "Mahalanobis distance", ylab ="Weights", pch=3)
 points(x = maha_T[cc==1], y = est_T$E00[cc==1], pch = 1, col = "red")
 abline(h = 1)
-plot(maha_ST[cc==0], est_ST$E00[cc==0], main="ST", xlab = "Mahalanobis distance", ylab ="Weights", pch=3)
+plot(maha_ST[cc==0], est_ST$E00[cc==0], xlim = c(min(maha_ST),max(maha_ST)), main="ST", xlab = "Mahalanobis distance", ylab ="Weights", pch=3)
 points(x = maha_ST[cc==1], y = est_ST$E00[cc==1], pch = 1, col = "red")
 abline(h = 1)
 
@@ -86,17 +84,17 @@ plot(If_N$Medida, ylim = c(0, 0.20), main ="N", xlab = "Index", ylab="M(0)", typ
 abline(h = If_N$Benchmark, lty = 3)
 plot(If_N2$Medida, ylim = c(0, 0.20), main ="N", xlab = "Index", ylab="M(0)", type="h")
 abline(h = If_N2$Benchmark, lty = 3)
-plot(If_SN$Medida, ylim = c(0, 0.20), main ="SN", xlab = "Index", ylab="M(0)", type="h")
+plot(If_SN$Medida, ylim = c(0, 0.10), main ="SN", xlab = "Index", ylab="M(0)", type="h")
 abline(h = If_SN$Benchmark, lty = 3)
-plot(If_SN2$Medida, ylim = c(0, 0.20), main ="SN", xlab = "Index", ylab="M(0)", type="h")
+plot(If_SN2$Medida, ylim = c(0, 0.10), main ="SN", xlab = "Index", ylab="M(0)", type="h")
 abline(h = If_SN2$Benchmark, lty = 3)
-plot(If_T$Medida, ylim = c(0, 0.06), main ="T", xlab = "Index", ylab="M(0)", type="h")
+plot(If_T$Medida, ylim = c(0, 0.04), main ="T", xlab = "Index", ylab="M(0)", type="h")
 abline(h = If_T$Benchmark, lty = 3)
-plot(If_T2$Medida, ylim = c(0, 0.06), main ="T", xlab = "Index", ylab="M(0)", type="h")
+plot(If_T2$Medida, ylim = c(0, 0.04), main ="T", xlab = "Index", ylab="M(0)", type="h")
 abline(h = If_T2$Benchmark, lty = 3)
-plot(If_ST$Medida, ylim = c(0, 0.06), main ="ST", xlab = "Index", ylab="M(0)", type="h")
+plot(If_ST$Medida, ylim = c(0, 0.04), main ="ST", xlab = "Index", ylab="M(0)", type="h")
 abline(h = If_ST$Benchmark, lty = 3)
-plot(If_ST2$Medida, ylim = c(0, 0.06), main ="ST", xlab = "Index", ylab="M(0)", type="h")
+plot(If_ST2$Medida, ylim = c(0, 0.04), main ="ST", xlab = "Index", ylab="M(0)", type="h")
 abline(h = If_ST2$Benchmark, lty = 3)
 
 #######################################################################
@@ -108,9 +106,9 @@ if(!require(astrodatR)) install.packages("astrodatR");  library(astrodatR)
 
 data(censor_Be)
 dados <- censor_Be
-y     <- dados[,5]
-cc    <- dados[,4]
-x1    <- dados[,3]/1000
+y     <- dados$logN_Be
+cc    <- ifelse(dados$Ind_Be==1, 0, 1) # cc == 1 represents the censored!
+x1    <- dados$Teff/1000
 x     <- cbind(1, x1)
 
 est_N  <- EM.skewCens(cc, x, y, show.envelope = "FALSE", family = "N")
@@ -133,20 +131,23 @@ benchmark_GD_SN <- 2*(p+2)/n
 benchmark_GD_T  <- 2*(p+1)/n 
 benchmark_GD_ST <- 2*(p+2)/n 
 
+round(c(min(CD_N$GD),max(CD_N$GD)),4)
+round(c(min(CD_SN$GD),max(CD_SN$GD)),4)
+round(c(min(CD_T$GD),max(CD_T$GD)),4)
+round(c(min(CD_ST$GD),max(CD_ST$GD)),4)
+
 # GD Plots
 par(mfrow=c(2,2))
-plot(CD_N$GD, ylim = c(0, 0.15), main="N", xlab="Index", ylab="GD", type="h")
+plot(CD_N$GD, ylim = c(0,0.12), main="N", xlab="Index", ylab="GD", type="h")
 abline(h = benchmark_GD_N, lty = 3)
 plot(CD_SN$GD, ylim = c(0, 0.12), main="SN", xlab="Index", ylab="GD", type="h")
 abline(h = benchmark_GD_SN, lty = 3)
-plot(CD_T$GD, ylim = c(0, 0.15), main="T", xlab="Index", ylab="GD", type="h")
+plot(CD_T$GD, ylim = c(0, 0.12), main="T", xlab="Index", ylab="GD", type="h")
 abline(h = benchmark_GD_T, lty = 3)
 plot(CD_ST$GD, ylim = c(0, 0.12), main="ST", xlab="Index", ylab="GD", type="h")
 abline(h = benchmark_GD_ST, lty = 3)
 
 ##  Mahalanobis versus Weights
-maha_N  <- (est_N$yhat -x%*%est_N$theta[1:p])^2/est_N$theta[p+1]
-maha_SN <- (est_SN$yhat-x%*%est_SN$theta[1:p])^2/est_SN$theta[p+1]
 maha_T  <- (est_T$yhat -x%*%est_T$theta[1:p])^2/est_T$theta[p+1]
 maha_ST <- (est_ST$yhat-x%*%est_ST$theta[1:p])^2/est_ST$theta[p+1]
 
@@ -173,19 +174,19 @@ If_ST2 <- If(cc,x,y,est_ST$theta,est_ST$E00,est_ST$E01,est_ST$E02,est_ST$E10,est
 
 ### Figure. Case-weight and Scale perturbation
 par(mfrow=c(4,2))
-plot(If_N$Medida, ylim = c(0, 0.30), main ="N", xlab = "Index", ylab="M(0)", type="h")
+plot(If_N$Medida, ylim = c(0, 0.60), main ="N", xlab = "Index", ylab="M(0)", type="h")
 abline(h = If_N$Benchmark, lty = 3)
-plot(If_N2$Medida, ylim = c(0, 0.30), main ="N", xlab = "Index", ylab="M(0)", type="h")
+plot(If_N2$Medida, ylim = c(0, 0.60), main ="N", xlab = "Index", ylab="M(0)", type="h")
 abline(h = If_N2$Benchmark, lty = 3)
-plot(If_SN$Medida, ylim = c(0, 1.00), main ="SN", xlab = "Index", ylab="M(0)", type="h")
+plot(If_SN$Medida, ylim = c(0, 0.70), main ="SN", xlab = "Index", ylab="M(0)", type="h")
 abline(h = If_SN$Benchmark, lty = 3)
 plot(If_SN2$Medida, ylim = c(0, 0.30), main ="SN", xlab = "Index", ylab="M(0)", type="h")
 abline(h = If_SN2$Benchmark, lty = 3)
-plot(If_T$Medida, ylim = c(0, 0.30), main ="T", xlab = "Index", ylab="M(0)", type="h")
+plot(If_T$Medida, ylim = c(0, 0.20), main ="T", xlab = "Index", ylab="M(0)", type="h")
 abline(h = If_T$Benchmark, lty = 3)
-plot(If_T2$Medida, ylim = c(0, 0.30), main ="T", xlab = "Index", ylab="M(0)", type="h")
+plot(If_T2$Medida, ylim = c(0, 0.20), main ="T", xlab = "Index", ylab="M(0)", type="h")
 abline(h = If_T2$Benchmark, lty = 3)
-plot(If_ST$Medida, ylim = c(0, 1.00), main ="ST", xlab = "Index", ylab="M(0)", type="h")
+plot(If_ST$Medida, ylim = c(0, 0.30), main ="ST", xlab = "Index", ylab="M(0)", type="h")
 abline(h = If_ST$Benchmark, lty = 3)
-plot(If_ST2$Medida, ylim = c(0, 0.30), main ="ST", xlab = "Index", ylab="M(0)", type="h")
+plot(If_ST2$Medida, ylim = c(0, 0.20), main ="ST", xlab = "Index", ylab="M(0)", type="h")
 abline(h = If_ST2$Benchmark, lty = 3)
